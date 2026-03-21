d447f6fdfc8e2ddf6bab6d6039a08ebbbc68838f
juhuihee <jbijou0419@gmail.com>
2026-03-21 23:59:09 +0900

    fix : 행렬 테두리 회전하기 회전 방향 및 로직 수정

--- programmers/level2/MatrixBorderRotation.java
+++ programmers/level2/MatrixBorderRotation.java
@@ -17,42 +17,40 @@ public class MatrixBorderRotation {
     int[] answer = new int[queries.length];
 
     for (int q = 0; q < queries.length; q++) {
-      int x1 = queries[q][0];
-      int y1 = queries[q][1];
-      int x2 = queries[q][2];
-      int y2 = queries[q][3];
+      int x1 = queries[q][0] - 1;
+      int y1 = queries[q][1] - 1;
+      int x2 = queries[q][2] - 1;
+      int y2 = queries[q][3] - 1;
 
       int min = Integer.MAX_VALUE;
-      int temp = board[x1 - 1][y1 - 1]; // 시작값 저장
+      int temp = board[x1][y2]; // 오른쪽 위 코너 저장
       min = Math.min(min, temp);
 
-      // 윗줄: 왼→오
-      for (int i = y1 - 1; i < y2 - 1; i++) {
-        board[x1 - 1][i] = board[x1 - 1][i + 1];
-        min = Math.min(min, board[x1 - 1][i]);
+      // 윗줄 : 오 -> 왼
+      for (int i = y2; i > y1; i--) {
+        board[x1][i] = board[x1][i - 1];
+        min = Math.min(min, board[x1][i]);
       }
 
-      // 오른줄: 위→아래
-      for (int i = x1 - 1; i < x2 - 1; i++) {
-        board[i][y2 - 1] = board[i + 1][y2 - 1];
-        min = Math.min(min, board[i][y2 - 1]);
+      // 왼줄 : 위 → 아래
+      for (int i = x1; i < x2; i++) {
+        board[i][y1] = board[i + 1][y1];
+        min = Math.min(min, board[i][y1]);
       }
 
-      // 아랫줄: 오→왼
-      for (int i = y2 - 1; i > y1 - 1; i--) {
-        board[x2 - 1][i] = board[x2 - 1][i - 1];
-        min = Math.min(min, board[x2 - 1][i]);
+      // 아랫줄 : 왼 -> 오
+      for (int i = y1; i < y2; i++) {
+        board[x2][i] = board[x2][i + 1];
+        min = Math.min(min, board[x2][i]);
       }
 
-      // 왼줄: 아래→위
-      for (int i = x2 - 1; i > x1 - 1; i--) {
-        board[i][y1 - 1] = board[i - 1][y1 - 1];
-        min = Math.min(min, board[i][y1 - 1]);
+      // 오른줄 : 아래 → 위
+      for (int i = x2; i > x1; i--) {
+        board[i][y2] = board[i - 1][y2];
+        min = Math.min(min, board[i][y2]);
       }
 
-      // temp(시작값)를 왼줄 맨 위 바로 아래에 넣기
-      board[x1][y1 - 1] = temp;
-      min = Math.min(min, temp);
+      board[x1 + 1][y2] = temp;
 
       answer[q] = min;
     }
@@ -60,10 +58,27 @@ public class MatrixBorderRotation {
   }
 
   public static void main(String[] args) {
-    MatrixBorderRotation m = new MatrixBorderRotation();
-    int[][] queries = { { 2, 2, 5, 4 }, { 3, 3, 6, 6 }, { 5, 1, 6, 3 } };
-    int[] result = m.solution(6, 6, queries);
-    for (int r : result)
-      System.out.print(r + " "); // 기댓값: 8 10 25
+    MatrixBorderRotation p = new MatrixBorderRotation();
+
+    int[][] queries1 = { { 2, 2, 5, 4 }, { 3, 3, 6, 6 }, { 5, 1, 6, 3 } };
+    int[] result1 = p.solution(6, 6, queries1);
+    System.out.print("테스트1: ");
+    for (int r : result1)
+      System.out.print(r + " ");
+    System.out.println("(기댓값: 8 10 25)");
+
+    int[][] queries2 = { { 1, 1, 2, 2 }, { 1, 2, 2, 3 }, { 2, 1, 3, 2 }, { 2, 2, 3, 3 } };
+    int[] result2 = p.solution(3, 3, queries2);
+    System.out.print("테스트2: ");
+    for (int r : result2)
+      System.out.print(r + " ");
+    System.out.println("(기댓값: 1 1 5 3)");
+
+    int[][] queries3 = { { 1, 1, 100, 97 } };
+    int[] result3 = p.solution(100, 97, queries3);
+    System.out.print("테스트3: ");
+    for (int r : result3)
+      System.out.print(r + " ");
+    System.out.println("(기댓값: 1)");
   }
 }
